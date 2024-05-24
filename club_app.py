304add3ca3c3fd30eaa57f9bdb5a533a888dfa40
SaitoKoshi <saitokoshi@SaitoKoshinoAir.AirPort>
2024-05-24 21:53:45 +0900

    session state

--- club_app.py
+++ club_app.py
@@ -5,11 +5,8 @@ import weather_functions as wf
 session_state = st.session_state
 
 # セッション状態が初期化されていない場合は初期化する
-if 'current_weather_info' not in session_state:
-    session_state.current_weather_info = None
-
-if 'tomorrow_weather_info' not in session_state:
-    session_state.tomorrow_weather_info = None
+if 'weather_info' not in session_state:
+    session_state.weather_info = {}
 
 # 地域名と緯度・経度の対応辞書
 locations = {
@@ -34,31 +31,33 @@ openweathermap_api_key = st.secrets['club']["API_KEY"]
 
 # 現在の天気情報を表示する領域
 st.header("現在の天気情報")
-if st.button("現在の天気情報を取得する",key=1):
+if st.button("現在の天気情報を取得する", key=1):
     if location:
         latitude = locations[location]["lat"]
         longitude = locations[location]["lon"]
         current_weather_info = wf.get_current_weather_info(latitude, longitude, openweathermap_api_key)
         if isinstance(current_weather_info, dict):
-            session_state.current_weather_info = current_weather_info
-        for key, value in session_state.current_weather_info.items():
-            st.write(f"{key}: {value}")
-        google_map_url = wf.generate_google_map_url(latitude, longitude)
-        st.write(f"Google Map URL: {google_map_url}")
+            session_state.weather_info[location] = current_weather_info
+            for key, value in session_state.weather_info[location].items():
+                st.write(f"{key}: {value}")
+            google_map_url = wf.generate_google_map_url(latitude, longitude)
+            st.write(f"Google Map URL: {google_map_url}")
+        else:
+            st.error("天気情報の取得に失敗しました。")
     else:
         st.warning("地域を選択してください。")
 
 # n時間後の天気情報を表示する領域
 st.header("n時間後の天気情報")
 hours_ahead = st.slider("何時間後の天気情報を取得しますか？", 0, 24, 0, step=3)
-if st.button(f"{hours_ahead}時間後の天気情報を取得する",key=2):
+if st.button(f"{hours_ahead}時間後の天気情報を取得する", key=2):
     if location:
         latitude = locations[location]["lat"]
         longitude = locations[location]["lon"]
         tomorrow_weather_info = wf.get_tomorrow_weather_info(latitude, longitude, openweathermap_api_key, hours_ahead)
         if isinstance(tomorrow_weather_info, dict):
-            session_state.tomorrow_weather_info = tomorrow_weather_info
-            for key, value in session_state.tomorrow_weather_info.items():
+            session_state.weather_info[location] = tomorrow_weather_info
+            for key, value in session_state.weather_info[location].items():
                 st.write(f"{key}: {value}")
             google_map_url = wf.generate_google_map_url(latitude, longitude)
             st.write(f"Google Map URL: {google_map_url}")
