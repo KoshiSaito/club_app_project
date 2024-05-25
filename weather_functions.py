e3eb7a2535ed2b3300f0c71835da71fb792c0968
KoshiSaito <saito.koshi.005@s.kyushu-u.ac.jp>
2024-05-25 09:57:43 +0900

    chore: Add matplotlib for weather visualization and refactor weather functions

--- weather_functions.py
+++ weather_functions.py
@@ -2,6 +2,8 @@ import requests
 from datetime import datetime, timedelta
 import random
 import streamlit as st
+import matplotlib.pyplot as plt
+
 
 # 関数定義
 def convert_wind_direction(deg):
@@ -85,3 +87,39 @@ def generate_google_map_url(latitude, longitude):
 def get_fortune():
     fortunes = ["大吉", "中吉", "小吉", "吉", "末吉", "凶", "大凶"]
     return f"今日の運勢: {fortunes[random.randint(0, len(fortunes)-1)]}"
+
+def visualize_weather_info(latitude, longitude, api_key):
+    url = "https://api.openweathermap.org/data/2.5/onecall"
+    params = {
+        "lat": latitude,
+        "lon": longitude,
+        "appid": api_key,
+        "units": "metric",
+        "lang": "ja",
+    }
+    response = requests.get(url, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        daily_weather = data['daily']
+        dates = []
+        max_temps = []
+        min_temps = []
+        weather_icons = []
+        for day in daily_weather:
+            dates.append(datetime.fromtimestamp(day['dt']).strftime('%m/%d'))
+            max_temps.append(day['temp']['max'])
+            min_temps.append(day['temp']['min'])
+            weather_icons.append(f"http://openweathermap.org/img/w/{day['weather'][0]['icon']}.png")
+        # グラフの描画
+        fig, ax = plt.subplots()
+        ax.plot(dates, max_temps, label='最高気温', marker='o')
+        ax.plot(dates, min_temps, label='最低気温', marker='o')
+        ax.set_xlabel('日付')
+        ax.set_ylabel('気温 (℃)')
+        ax.set_title('天気予報')
+        ax.legend()
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.savefig("weather_info.png")
+    else:
+        st.error("Failed to retrieve weather information.")
